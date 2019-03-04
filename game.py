439e1bc779fed36c24f56641e7b92d81370566bf
Kubilay İşen <34842742+isennkubilay@users.noreply.github.com>
2019-03-04 14:33:19 +0300

    Create game.py

---
+++ game.py
@@ -0,0 +1,40 @@
+
+from random import randint
+
+print("""
+	Rock...
+	Paper...
+	Scissors...""")
+
+
+player = input("Player, make your move: ").lower()
+
+rand_num = randint(0,2)
+if rand_num == 0:
+	computer = "scissors"
+elif rand_num == 1:
+	computer = "rock"
+else:
+	computer = "paper"
+
+print(f"Computer plays {computer}" )
+
+if player == computer:
+	print("It's a tie!")
+elif player == "rock":
+	if computer == "scissors":
+		print("player wins!")
+	else:
+		print("computer wins!")
+elif player == "paper":
+	if computer == "rock":
+		print("player wins!")
+	else:
+		print("computer wins!")
+elif player == "scissors":
+	if computer == "paper":
+		print("player wins!")
+	else:
+		print("computer wins!")	
+else:
+	print("Please enter a valid move!")
